b91d562d59605417680e1ac606c099205c3dbfea
rui-exe <mrswager173@gmail.com>
2023-05-28 22:49:19 +0100

    limits for iinc

--- src/main/pt/up/fe/comp2023/jasmin/OllirVisitorForJasmin.java
+++ src/main/pt/up/fe/comp2023/jasmin/OllirVisitorForJasmin.java
@@ -243,12 +243,18 @@ public class OllirVisitorForJasmin {
             Operand destOperand = (Operand) assign.getDest();
             if(((BinaryOpInstruction) rhs).getLeftOperand() instanceof Operand && destOperand.getName().equals(((Operand) ((BinaryOpInstruction) rhs).getLeftOperand()).getName()) && ((BinaryOpInstruction) rhs).getRightOperand().isLiteral()) {
                 if (((BinaryOpInstruction) rhs).getOperation().getOpType() == ADD) {
-                    result.append("\tiinc " + localVariable.get(destOperand.getName()).getVirtualReg() + " " + ((LiteralElement) ((BinaryOpInstruction) rhs).getRightOperand()).getLiteral() + "\n");
-                    return result;
+                    if (Integer.parseInt(((LiteralElement) ((BinaryOpInstruction) rhs).getRightOperand()).getLiteral())<= 127) {
+                        result.append("\tiinc " + localVariable.get(destOperand.getName()).getVirtualReg() + " " + ((LiteralElement) ((BinaryOpInstruction) rhs).getRightOperand()).getLiteral() + "\n");
+                        return result;
+                    }
+                    else result.append(visitBinaryOpInstruction((BinaryOpInstruction) rhs, localVariable));
                 }
                 else if (((BinaryOpInstruction) rhs).getOperation().getOpType() == SUB) {
-                    result.append("\tiinc " + localVariable.get(destOperand.getName()).getVirtualReg() + " -" + ((LiteralElement) ((BinaryOpInstruction) rhs).getRightOperand()).getLiteral() + "\n");
-                    return result;
+                    if (Integer.parseInt(((LiteralElement) ((BinaryOpInstruction) rhs).getRightOperand()).getLiteral())<= 128) {
+                        result.append("\tiinc " + localVariable.get(destOperand.getName()).getVirtualReg() + " -" + ((LiteralElement) ((BinaryOpInstruction) rhs).getRightOperand()).getLiteral() + "\n");
+                        return result;
+                    }
+                    else result.append(visitBinaryOpInstruction((BinaryOpInstruction) rhs, localVariable));
                 }
                 else {
                     result.append(visitBinaryOpInstruction((BinaryOpInstruction) rhs, localVariable));
@@ -256,8 +262,11 @@ public class OllirVisitorForJasmin {
             }
             else if(((BinaryOpInstruction) rhs).getRightOperand() instanceof Operand && destOperand.getName().equals(((Operand) ((BinaryOpInstruction) rhs).getRightOperand()).getName()) && ((BinaryOpInstruction) rhs).getLeftOperand().isLiteral()) {
                 if (((BinaryOpInstruction) rhs).getOperation().getOpType() == ADD) {
-                    result.append("\tiinc " + localVariable.get(destOperand.getName()).getVirtualReg() + " " + ((LiteralElement) ((BinaryOpInstruction) rhs).getLeftOperand()).getLiteral() + "\n");
-                    return result;
+                    if (Integer.parseInt(((LiteralElement) ((BinaryOpInstruction) rhs).getLeftOperand()).getLiteral())<= 127) {
+                        result.append("\tiinc " + localVariable.get(destOperand.getName()).getVirtualReg() + " " + ((LiteralElement) ((BinaryOpInstruction) rhs).getLeftOperand()).getLiteral() + "\n");
+                        return result;
+                    }
+                    else result.append(visitBinaryOpInstruction((BinaryOpInstruction) rhs, localVariable));
                 }
                 else {
                     result.append(visitBinaryOpInstruction((BinaryOpInstruction) rhs, localVariable));
